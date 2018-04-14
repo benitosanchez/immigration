b55f74a140d60aa2215429fa2927c57c1b9907ef
Benito Sanchez <sanchezbenitog@gmail.com>
2018-04-14 09:50:27 -0700

    Adding Default Android Activity

---
+++ immigration/app/src/main/java/ai/immigration/immigration/questionsScreen/QuestionsActivity.java
@@ -0,0 +1,15 @@
+package ai.immigration.immigration.questionsScreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ai.immigration.immigration.R;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quesetions);
+    }
+}
